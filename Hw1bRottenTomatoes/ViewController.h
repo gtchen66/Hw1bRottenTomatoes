53d42b32e8a5fc369af2eef54c45ca97b374bf95
gtchen66 <gtchen66@yahoo.com>
2014-01-08 00:29:21 -0800

    Initial Commit

---
+++ Hw1bRottenTomatoes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hw1bRottenTomatoes
+//
+//  Created by George Chen on 1/8/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
